90a317b515c2d198e4c71e94ec108b07eebd0d90
xiaolaba <xiao_laba_cn@yahoo.com>
2022-06-20 22:47:18 +0800

    Update burn.bat

--- burn.bat
+++ burn.bat
@@ -11,9 +11,14 @@ set ac=C:\avr8-gnu-toolchain-win32_x86
 path %ac%\bin;%ac%\utils\bin;%path%;
 avr-gcc.exe -dumpversion
  
+:: factory default 
 set efuse=0xf7
-set hfuse=0xd4
-set lfuse=0x52
+set hfuse=0xd9
+set lfuse=0x62
+
+::set efuse=0xf7
+::set hfuse=0xd4
+::set lfuse=0x52
 set lock=0x3f
 set sig=0x1e,0x95,0x16
  
